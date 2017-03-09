a837ec65a40ae01cacc32d08a894dcd668414093
Matt Catha <mattcatha@gmail.com>
2017-03-09 11:59:13 -0600

    Added comments

--- php/updateLikes.php
+++ php/updateLikes.php
@@ -24,16 +24,20 @@
 		// Convert from string to boolean
 		$isLiked === "true" ? $isLiked = true : $isLiked = false;
 
+		// If it is already liked, decrease count in the database and change "liked" status to "like"
 		if ($isLiked) {
 			$updateLikesQuery = "UPDATE `projectlikes` SET likes = likes - 1 WHERE `projectname` = '$name' LIMIT 1";
 			
 			$updateStatusQuery = "UPDATE `userlikes` SET $name = 0 WHERE `username` = '$user' LIMIT 1";
+		
+		// Else add one to the like count and change status to "liked" in database
 		} else {
 			$updateLikesQuery = "UPDATE `projectlikes` SET likes = likes + 1 WHERE `projectname` = '$name' LIMIT 1";
 
 			$updateStatusQuery = "UPDATE `userlikes` SET $name = 1 WHERE `username` = '$user' LIMIT 1";
 		}
 		
+		// Run both queries
 		mysqli_query($link, $updateLikesQuery);
 		mysqli_query($link, $updateStatusQuery);
 	}
